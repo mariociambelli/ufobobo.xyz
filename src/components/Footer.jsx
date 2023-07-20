b0ca761548529968f6a2bc5cead1388fecc1a9ed
Mario Ciambelli <hello@bottegaduepuntozero.it>
2023-07-20 14:48:20 +0200

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -17,30 +17,30 @@ export function Footer() {
   return (
     <footer className="mt-32">
       <Container.Outer>
-        <div className="border-t border-zinc-200 pb-16">
+        <div className="">
           <Container.Inner>
-            <div className="flex flex-col items-center justify-between gap-3 sm:flex-row md:gap-6">
-              <div className="flex gap-6 text-sm text-zinc-400">
+            <div className="flex flex-col items-center justify-between gap-3 border-t  border-zinc-200 sm:flex-row md:gap-6">
+              <div className="flex gap-6 pt-8 text-sm text-zinc-400">
                 <a
                   href="https://twitter.com/MarioCiambelli"
-                  className="hover:text-black"
+                  className="text-black hover:text-zinc-400"
                 >
                   Twitter
                 </a>
                 <a
                   href="https://github.com/mariociambelli"
-                  className="hover:text-black"
+                  className="text-black hover:text-zinc-400"
                 >
                   GitHub
                 </a>
                 <a
                   href="mailto:mario@spearbit.com"
-                  className="hover:text-black"
+                  className="text-black hover:text-zinc-400"
                 >
                   Mail
                 </a>
               </div>
-              <p className="text-sm text-zinc-400">
+              <p className="pt-0 text-sm text-zinc-400  md:pt-8">
                 &copy; {new Date().getFullYear()} Ufobobo - All rights reserved.
               </p>
             </div>
