e791f9d263fcc8ff994abffb07671bbdd5477a4a
Mario Ciambelli <hello@bottegaduepuntozero.it>
2025-08-02 11:02:59 +0200

    Small changes

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -33,22 +33,12 @@ export default function Home({ articles }) {
       </Head>
       <Container className="mario-bg m-auto flex items-center bg-zinc-100 pt-12">
         <div className="">
-          <div className="max-w-6xl text-center">
-            <div className="mx-auto w-72">
-              <motion.div
-                whileHover={{
-                  scale: 1.05,
-                  transition: { duration: 1 },
-                }}
-              >
-                <Avatar />
-              </motion.div>
-            </div>
+          <div className="max-w-6xl text-left">
             <div className="mt-6">
-              <h1 className="bold text-5xl text-zinc-900 md:text-7xl">
+              <h1 className="bold text-9xl text-zinc-900 md:text-9xl">
                 I'm Mario <i>aka</i> Ufobobo
               </h1>
-              <h2 className="mt-6 text-xl text-zinc-500">
+              <h2 className="mt-6 text-3xl text-zinc-500">
                 Currently Product Designer{' '}
                 <a href="https://spearbit.com/" className="hover:text-black">
                   @Spearbit
